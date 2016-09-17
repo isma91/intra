e62a47cd69845a6bd4adb13a6417c59a14d10614
isma91 <ismaydogmus@gmail.com>
2016-09-17 22:54:10 +0200

    the sample of the config file

---
+++ config-sample.php
@@ -0,0 +1,23 @@
+<?php
+/**
+* Config-sample.php
+*
+* This is a sample config file, if you install and the config.php file doesn't create,
+* you can rename this file to config.php and change the host, dbname, user, password
+* change install to true to use this project
+*
+* PHP Version 7.0.8
+*
+* @category Config
+* @author   isma91 <ismaydogmus@gmail.com>
+* @license  https://opensource.org/licenses/mit-license.php MIT License
+*/
+return array(
+    array(
+			'host' => 'localhost',
+			'dbname' => 'intra',
+			'user' => 'root',
+			'password' => '',
+    ),
+	"install" => false
+);
