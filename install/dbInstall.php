ffdd239362eef8baf8fc10171707f2b37c8f4c90
isma91 <ismaydogmus@gmail.com>
2016-09-17 22:57:12 +0200

    create the empty database

---
+++ install/dbInstall.php
@@ -0,0 +1,44 @@
+<?php
+/**
+* Install_db.php
+*
+* Create the database
+*
+* PHP Version 7.0.8
+*
+* @category Model
+* @author   isma91 <ismaydogmus@gmail.com>
+* @license  https://opensource.org/licenses/mit-license.php MIT License
+*/
+$duplicateDb = false;
+try {
+	$db = new PDO("mysql:host=" . $_POST["host"] . ";", $_POST["username"], $_POST["password"]);
+	$dbRequest = $bdd->prepare("SHOW DATABASES;");
+	$dbRequest->execute();
+	$dbData = $dbRequest->fetchAll();
+	foreach ($dbData as $value) {
+		if ($value["Database"] === $_POST["dbname"]) {
+			$duplicateDb = true;
+			break;
+		}
+	}
+	if ($duplicateDb === true) {
+		echo "Error !! The database '" . $_POST["dbname"] . "' already exist !!\n";
+	} else {
+		$bdd->exec("CREATE DATABASE IF NOT EXISTS `" . $_POST["dbname"] . "`;");
+		echo "Database '" . $_POST["dbname"] . "' successfully created !!\n";
+	}
+} catch (PDOException $exception) {
+        if ($exception->getCode() === 2005) {
+            echo "Error !! The MySQL server '" . $_POST["host"] . "' is not recognized !!\n";
+        } elseif ($exception->getCode() === 1045) {
+            if ($_POST["password"] === "") {
+                echo "Error !! The MySQL server denied acces to '" . $_POST["username"] . "', maybe you forgot to write the password ??\n";
+            } else {
+                echo "Error !! The MySQL server denied acces to '" . $_POST["username"] . "', maybe you have write the wrong password ??\n";
+            }
+        } else {    
+            echo "Error !! " . $exception->getMessage();
+        }
+    }
+?>
