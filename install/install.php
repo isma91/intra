19819094658797bee80519411ad4f2030e172435
isma91 <ismaydogmus@gmail.com>
2016-09-21 17:21:57 +0200

    change field name

--- install/install.php
+++ install/install.php
@@ -20,7 +20,6 @@ if (file_exists('./../config.php')) {
 </head>
 <body>
 <div class="container">
-	<div id="event"></div>
 	<div class="mui-panel install">
 		<p>Welcome to intra installation !!</p>
 		<p>If you're ready to go, click to the install button !!</p>
@@ -30,67 +29,59 @@ if (file_exists('./../config.php')) {
 		</button>
 	</div>
 	<div class="loader"></div>
-	<div class="mui-panel">
+	<div class="mui-panel formInstall">
 		<div class="row divFormInstall">
 			<h2 class="title">Your Admin Profile</h2>
 			<div class="input-field input_install">
-				<input id="last_name" type="text">
-				<label>Lastname</label>
+				<input id="lastname" type="text">
+				<label for="lastname">Lastname</label>
 			</div>
 			<div class="input-field input_install">
-				<input id="first_name" type="text">
-				<label>Firstname</label>
+				<input id="firstname" type="text">
+				<label for="firstname">Firstname</label>
 			</div>
 			<div class="input-field input_install_full">
-				<input id="pseudo" type="text">
-				<label>Nickname</label>
+				<input id="nickname" type="text">
+				<label for="nickname">Nickname</label>
 			</div>
 			<div class="input-field input_install">
 				<input id="password" type="password">
-				<label id="labelPassword">Password<span id="spanLabelPassword"></span></label>
+				<label id="labelPassword" for="password">Password<span id="spanLabelPassword"></span></label>
 			</div>
 			<div class="input-field input_install">
 				<input id="confirmPassword" type="password">
-				<label>Confirm Password<span id="spanLabelConfirmPassword"></span></label>
+				<label for="confirmPassword">Confirm Password<span id="spanLabelConfirmPassword"></span></label>
 			</div>
 			<div class="input-field input_install_full">
 				<input id="email" type="text">
-				<label>Email<span id="spanLabelEmail"></span></label>
+				<label for="email">Email<span id="spanLabelEmail"></span></label>
 			</div>
 		</div>
-		<button class="waves-effect btn-flat nextStepInstall" disabled>
-			Next Step
-		</button>
+		<button class="waves-effect btn-flat nextStepInstall" disabled>Next Step</button>
 	</div>
 	<div class="mui-panel dbInstall">
 		<div class="row">
-			<h2>Database installation</h2>
+			<h2 class="title">Database installation</h2>
 			<div class="input-field input_install_full">
 				<input id="host" type="text" value="localhost">
-				<label>Host</label>
+				<label for="host">Host</label>
 			</div>
 			<div class="input-field input_install_full">
 				<input id="username" type="text" value="root">
-				<label>Username</label>
+				<label for="username">Username</label>
 			</div>
 			<div class="input-field input_install_full">
 				<input id="dbPassword" type="password">
-				<label>Password</label>
+				<label for="dbPassword">Password</label>
 			</div>
 			<div class="input-field input_install">
 				<input id="dbname" type="text" value="intra">
-				<label>Database Name</label>
+				<label for="dbname">Database Name</label>
 			</div>
 			<div class="endButton">
-				<button class="waves-effect btn-flat" id="createDb">
-					Create database
-				</button>
-				<button class="waves-effect btn-flat" id="checkDb">
-					Try connection
-				</button>
-				<button class="waves-effect btn-flat" id="finishInstall" disabled>
-					Finish install
-				</button>
+				<button class="waves-effect btn-flat" id="createDb">Create database</button>
+				<button class="waves-effect btn-flat" id="checkDb">Try connection</button>
+				<button class="waves-effect btn-flat" id="finishInstall" disabled>Finish install</button>
 			</div>
 		</div>
 	</div>
