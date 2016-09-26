1d241b54fc54bdb6daa80ef43f7dae951875b271
isma91 <ismaydogmus@gmail.com>
2016-09-26 17:23:34 +0200

    remove console.log

--- install/install.js
+++ install/install.js
@@ -101,8 +101,6 @@ $(document).ready(function(){
 		dbName = $("input#dbname").val();
 		$.post( "dbInstall.php", {host: host, username: username, password: dbPassword, dbname: dbName}, function (data) {
 			var dataError;
-			console.log(data);
-			return;
 			dataError = false;
 			if (data.substr(0, 5) === "Error") {
 				Materialize.toast('<p class="alert-failed">' + data + '<p>', 3000, 'rounded alert-failed');
