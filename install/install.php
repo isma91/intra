8bd5227ce05f07d5a84118919fbb2d8d104ae69b
isma91 <ismaydogmus@gmail.com>
2016-10-31 12:05:29 +0100

    add favicon + js files

--- install/install.php
+++ install/install.php
@@ -10,6 +10,23 @@ if (!empty($configFile) || !empty($configFile['install'])) {
 	<meta charset="utf-8">
 	<meta name="description" content="Create your intra !!" />
 	<title>intra intallation</title>
+	<link rel="apple-touch-icon" sizes="57x57" href="../media/img/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="../media/img/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="../media/img/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="../media/img/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="../media/img/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="../media/img/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="../media/img/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="../media/img/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="../media/img/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="../media/img/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="../media/img/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="../media/img/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="../media/img/favicon-16x16.png">
+	<link rel="manifest" href="../media/img/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
 	<link media="all" type="text/css" rel="stylesheet" href="../media/css/google_material_icons.css" />
 	<link media="all" type="text/css" rel="stylesheet" href="../media/css/mui.min.css" />
 	<link media="all" type="text/css" rel="stylesheet" href="../media/css/materialize.min.css" />
@@ -18,6 +35,10 @@ if (!empty($configFile) || !empty($configFile['install'])) {
 	<script src="../media/js/mui.min.js"></script>
 	<script src="../media/js/materialize.min.js"></script>
 	<script src="install.js"></script>
+	<!--[if lt IE 9]>
+	<script src="../media/js/html5shiv.js"></script>
+	<script src="../media/js/respond.min.js"></script>
+	<![endif]-->
 </head>
 <body>
 <div class="container">
