412c353ad8435b1a4646453b668041f28add7b79
Ismail Aydogmus <ismaydogmus@gmail.com>
2016-11-08 14:33:08 +0100

    Use the new routing system and more condition to check the config file

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
 <?php
 session_start();
-$configFile = include('config.php');
-if (empty($configFile) || $configFile['install'] === false) {
+require 'autoload.php';
+/*
+ * If there is no selected language, we choose the fr
+ */
+//$_SESSION["lang"] = "fr";
+if (!isset($_SESSION["lang"])) {
+    $_SESSION["lang"] = "fr";
+}
+$configFile = include('./config.php');
+$router = new \routing\Router($_GET["url"]);
+/*
+ * long condition to check if the config file is truly empty, can grow with time
+ */
+if (!file_exists("./config.php") || empty($configFile) || !is_array($configFile) || empty($configFile["install"]) || $configFile['install'] === false) {
     header('Location: ./install/install.php');
 } else {
-    include './view/login.php';
+    $router = new \routing\Router($_GET["url"]);
+    $router->get("/", "site#login");
+    /*
+     * Run the routing System
+     */
+    $router->run();
 }
-require 'autoload.php';
